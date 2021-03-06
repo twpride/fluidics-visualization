1ffe04f0f492c4a26a1259516a3985b7837234cd
howard <twpride@gmail.com>
2021-03-05 19:23:48 -0800

    rename build

---
+++ doc/precache-manifest.1696eacc3357bb21ee4a90a0f9709f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a932ad5e2a1b7d81b8d219fd5e659967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a17571bf013221beb4",
+    "url": "/static/css/main.4093d2b6.chunk.css"
+  },
+  {
+    "revision": "780c8d3e1bd502325822",
+    "url": "/static/js/2.1f413edc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f413edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a17571bf013221beb4",
+    "url": "/static/js/main.3597074b.chunk.js"
+  },
+  {
+    "revision": "9df2b08bcd18a7a3e003",
+    "url": "/static/js/runtime-main.40047354.js"
+  }
+]);
